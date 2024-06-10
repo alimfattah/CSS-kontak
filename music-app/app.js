343c041d85c2ae06af45bf2ac66d27b7957b30f8
alimfattah <alim.fattah91@gmail.com>
2024-06-10 11:11:05 +0700

    add function

--- music-app/app.js
+++ music-app/app.js
@@ -2,18 +2,18 @@ const songData = [
     {
         name:"perfect Day",
         artist:"supercell",
-        src:"04 Perfect Day.flac"
-    }
+        src:"04 Perfect Day.flac",
+    },
     {
         name:"yume no sakai",
         artist:"supercell",
-        src:"03 yume no sakai.flac"
-    }
+        src:"03 yume no sakai.flac",
+    },
     {
         name:"utakata hanabi",
         artist:"supercell",
-        src:"02 utakata hanabi.flac"
-    }
+        src:"02 utakata hanabi.flac",
+    },
 ]
 
 const contain = document.querySelector(".contain")
@@ -43,18 +43,20 @@ const loadlagu = (index) =>{
     namapenyani.textContent=songData[index].artist
     judul.textContent=songData[index].name
     penyanyi.textContent=songData[index].artist
-    audio.src = 'music\${songData[index].src}.flac'
+    audio.src = 'music/${songData[index].src}'
 }
 
 const playlagu=() => {
     contain.classList.add("pause")
   tblplaypause.firstElementChild.className ='fa-solid fa-pause'
   audio.play()
+  cover.classList.add('rotate')
 }
 const pauselagu=() => {
     contain.classList.remove("pause")
   tblplaypause.firstElementChild.className ='fa-solid fa-play'
   audio.pause()
+  cover.classList.add('remove')
 }
 
 tblplaypause.addEventListener("click", () => {
@@ -64,6 +66,68 @@ tblplaypause.addEventListener("click", () => {
     {
         playlagu()
     }
+})
+
+
+const prevlagu = () => {
+    songIndex--
+    if(songIndex<0){
+        songIndex = songData.length -1
+    }
+    loadlagu(songIndex)
+    playlagu()
 }
 
-)
+const nextlagu = () => {
+    songIndex++
+    if(songIndex> songData.length - 1){
+        songIndex = 0
+    }
+    loadlagu(songIndex)
+    playlagu()
+}
+
+tblprev.addEventListener("click",prevlagu)
+tblnext.addEventListener("click",nextlagu)
+
+audio.addEventListener("timeupdate", (e) => {
+    const currentime = e.target.currentime
+    const durasi = e.target.durasi
+    let currentimewidth = (currentime/durasi)* 100
+    songprogress.style.width = '${currentimewidth}%'
+    let songcurrentime = document.querySelector(".timespan:nth-child(1)")
+    let songdurasi = document.querySelector(".timespan:nth-child(2)")
+
+    audio.addEventListener("loadedata", () =>{
+        let audiodurasi = audio.durasi
+
+        let totalMinute = Math.floor(audiodurasi/60)
+        let totalSeconds = Math.floor(audiodurasi % 60)
+
+        if(totalSeconds < 10){
+            totalSeconds = '0${totalSeconds}'
+        }
+        songdurasi.textContent = '${totalMinutes}:${totalSeconds}'
+        
+    })
+
+    let currentMinutes = Math.floor(currentime/ 60)
+        let currentSeconds = Math.floor(currentime%60)
+
+        if(currentSeconds < 10){
+            currentSeconds = '0${currentSeconds}'
+        }
+        songcurrentime.textContent='${currentMinutes}:${currentSeconds}'
+})
+
+
+songTime.addEventListener("click", (e) =>{
+    let progresWidth = songTime.clientWidth
+    let clickedoffsetX = e.offsetX
+    let songdurasi = audio.durasi
+    audio.currentime = (clickedoffsetX/progresWidth)* songdurasi
+    playlagu()
+} )
+
+
+audio.addEventListener('tamat',nextlagu)
